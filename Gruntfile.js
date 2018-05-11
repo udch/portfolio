75b99174fe0438813934e072662d7beaf7f6a892
Devi Chevuri <devi.chevuri@icidigital.com>
2018-05-11 00:12:00 -0500

    Added markup and styles

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+grunt.loadNpmTasks('grunt-responsive-images');
+
+grunt.initConfig({
+  responsive_images: {
+    options: {
+      // Task-specific options go here.
+    },
+    your_target: {
+      // Target-specific file lists and/or options go here.
+    },
+  },
+})
